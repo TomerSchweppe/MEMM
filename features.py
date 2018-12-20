add loss derivative and styling fixes

features.py
@@ -1,31 +1,32 @@
-
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import numpy as np
 from scipy import sparse
 import time
 
+
 class Feature:
     """
     base feature class
     """
-    def __init__(self,vocab_list,tag_list):
+
+    def __init__(self, vocab_list, tag_list):
         """save vocabulary list and tag list"""
         self._vocab_list = vocab_list
         self._tag_list = tag_list
-        self._word_idx_dict = {word: idx for idx,word in enumerate(vocab_list)}
+        self._word_idx_dict = {word: idx for idx, word in enumerate(vocab_list)}
         self._tag_idx_dict = {tag: idx for idx, tag in enumerate(tag_list)}
 
     def __call__(self):
         """call function"""
         pass
 
-    def feature_vec(self,shift,step,tag):
-        vec = np.zeros([step*len(self._tag_list)],dtype=bool)
-        tag_idx = self._tag_idx_dict.get(tag,None)
+    def feature_vec(self, shift, step, tag):
+        vec = np.zeros([step * len(self._tag_list)], dtype=bool)
+        tag_idx = self._tag_idx_dict.get(tag, None)
         if shift is None or tag_idx is None:
             return vec
-        pos = shift + tag_idx*step
+        pos = shift + tag_idx * step
         vec[pos] = 1
         return vec
 
@@ -36,22 +37,24 @@ class F100(Feature):
     F106 is similar -> call with previous word
     F107 is similar -> call with next word
     """
-    def __init__(self, vocab_list,tag_list):
-        """create word hash table"""
-        super(F100,self).__init__(vocab_list,tag_list)
 
-    def __call__(self, word,tag):
+    def __init__(self, vocab_list, tag_list):
+        """create word hash table"""
+        super(F100, self).__init__(vocab_list, tag_list)
+
+    def __call__(self, word, tag):
         """return F100/F106/F107 feature matrix"""
-        return self.feature_vec(self._word_idx_dict.get(word,None),len(self._word_idx_dict),tag)
+        return self.feature_vec(self._word_idx_dict.get(word, None), len(self._word_idx_dict), tag)
 
 
 class F101(Feature):
     """
     spelling features for all prefix of length n
     """
+
     def __init__(self, vocab_list, tag_list, n):
         """create prefix hash table"""
-        super(F101,self).__init__(vocab_list, tag_list)
+        super(F101, self).__init__(vocab_list, tag_list)
         assert n > 0
         self._n = n
 
@@ -61,17 +64,19 @@ class F101(Feature):
                 prefix_set.add(w[:self._n])
         self._prefix_to_idx = {prefix: idx for idx, prefix in enumerate(list(prefix_set))}
 
-    def __call__(self, word,tag):
+    def __call__(self, word, tag):
         """return F101 feature matrix"""
-        return self.feature_vec(self._prefix_to_idx.get(word[:self._n],None),len(self._prefix_to_idx),tag)
+        return self.feature_vec(self._prefix_to_idx.get(word[:self._n], None), len(self._prefix_to_idx), tag)
+
 
 class F102(Feature):
     """
     spelling features for all suffixes of length n
     """
+
     def __init__(self, vocab_list, tag_list, n):
         """create suffix hash table"""
-        super(F102,self).__init__(vocab_list, tag_list)
+        super(F102, self).__init__(vocab_list, tag_list)
         assert n > 0
         self._n = n
 
@@ -81,112 +86,120 @@ class F102(Feature):
                 suffix_set.add(w[-self._n:])
         self._suffix_to_idx = {suffix: idx for idx, suffix in enumerate(list(suffix_set))}
 
-    def __call__(self, word,tag):
+    def __call__(self, word, tag):
         """return F102 feature matrix"""
-        return self.feature_vec(self._suffix_to_idx.get(word[-self._n:], None),len(self._suffix_to_idx),tag)
+        return self.feature_vec(self._suffix_to_idx.get(word[-self._n:], None), len(self._suffix_to_idx), tag)
+
 
 class F103(Feature):
     """
     2 previous & current word tag
     """
+
     def __init__(self, vocab_list, tag_list):
         """create tag pairs hash table"""
         super(F103, self).__init__(vocab_list, tag_list)
-        self._tags_idx_dict = {(tag1,tag2): idx2+idx1*len(tag_list) for idx1, tag1 in enumerate(tag_list) for idx2, tag2 in enumerate(tag_list)}
+        self._tags_idx_dict = {(tag1, tag2): idx2 + idx1 * len(tag_list) for idx1, tag1 in enumerate(tag_list) for
+                               idx2, tag2 in enumerate(tag_list)}
 
-    def __call__(self,t_2,t_1,tag):
+    def __call__(self, t_2, t_1, tag):
         "return F103 feature matrix"
-        return self.feature_vec(self._tags_idx_dict.get((t_2,t_1),None),len(self._tag_list)*len(self._tag_list),tag)
+        return self.feature_vec(self._tags_idx_dict.get((t_2, t_1), None), len(self._tag_list) * len(self._tag_list),
+                                tag)
+
 
 class F104(Feature):
     """
     previous & current word tag 
     """
+
     def __init__(self, vocab_list, tag_list):
         """create tag hash table"""
         super(F104, self).__init__(vocab_list, tag_list)
         self._tag_idx_dict = {tag: idx for idx, tag in enumerate(tag_list)}
 
-    def __call__(self,t_1,tag):
+    def __call__(self, t_1, tag):
         """return F104 feature matrix"""
-        return self.feature_vec(self._tag_idx_dict.get(t_1,None),len(self._tag_list),tag)
+        return self.feature_vec(self._tag_idx_dict.get(t_1, None), len(self._tag_list), tag)
+
 
 class F105(Feature):
     """
     word tag
     """
+
     def __init__(self, vocab_list, tag_list):
         """init class"""
-        super(F105,self).__init__(vocab_list, tag_list)
+        super(F105, self).__init__(vocab_list, tag_list)
 
-    def __call__(self,tag):
+    def __call__(self, tag):
         """return F105 feature matrix"""
-        return self.feature_vec(0,1,tag)
+        return self.feature_vec(0, 1, tag)
+
 
 def sparse_vec_hstack(vec_list):
     """
     return sparse vector after hstack on numpy vector list
     """
-    return sparse.csr_matrix(np.concatenate(vec_list),dtype=bool)
-
+    return sparse.csr_matrix(np.concatenate(vec_list), dtype=bool)
 
 
 if __name__ == '__main__':
     """
     validation
     """
-    vocab_list = ['tomer','ofir','roy','nadav']
-    tag_list = ['S','T']
+    vocab_list = ['tomer', 'ofir', 'roy', 'nadav']
+    tag_list = ['S', 'T']
 
     # create all features
     n = 1
-    f_100 = F100(vocab_list,tag_list)
-    f_101 = F101(vocab_list,tag_list,n)
-    f_102 = F102(vocab_list,tag_list,n)
-    f_103 = F103(vocab_list,tag_list)
-    f_104 = F104(vocab_list,tag_list)
-    f_105 = F105(vocab_list,tag_list)
+    f_100 = F100(vocab_list, tag_list)
+    f_101 = F101(vocab_list, tag_list, n)
+    f_102 = F102(vocab_list, tag_list, n)
+    f_103 = F103(vocab_list, tag_list)
+    f_104 = F104(vocab_list, tag_list)
+    f_105 = F105(vocab_list, tag_list)
 
-    #test F100
+    # test F100
     assert np.array_equal(np.array([1, 0, 0, 0, 0, 0, 0, 0]),
-                                    f_100('tomer','S'))
+                          f_100('tomer', 'S'))
     assert np.array_equal(np.array([0, 1, 0, 0, 0, 0, 0, 0]),
-                                    f_100('ofir','S'))
+                          f_100('ofir', 'S'))
     assert np.array_equal(np.array([0, 0, 0, 0, 0, 0, 1, 0]),
-                                    f_100('roy','T'))
+                          f_100('roy', 'T'))
     assert np.array_equal(np.array([0, 0, 0, 0, 0, 0, 0, 1]),
-                                    f_100('nadav','T'))
+                          f_100('nadav', 'T'))
     # # test F101
-    assert np.array_equal(f_101('tomer','S'), f_101('tomer' + 'not_prefix','S'))
-    assert np.array_equal(f_101('tomer','S'), f_101('t','S'))
+    assert np.array_equal(f_101('tomer', 'S'), f_101('tomer' + 'not_prefix', 'S'))
+    assert np.array_equal(f_101('tomer', 'S'), f_101('t', 'S'))
     assert not np.array_equal(f_101('tomer', 'S'), f_101('t', 'T'))
-    assert not np.array_equal(f_101('tomer','S'), f_101('ofir','S'))
+    assert not np.array_equal(f_101('tomer', 'S'), f_101('ofir', 'S'))
     # # test F102
-    assert np.array_equal(f_102('tomer','S'), f_102('not_suffix' + 'tomer','S'))
-    assert np.array_equal(f_102('tomer','S'), f_102('r','S'))
-    assert np.array_equal(f_102('tomer','S'), f_102('ofir','S'))
+    assert np.array_equal(f_102('tomer', 'S'), f_102('not_suffix' + 'tomer', 'S'))
+    assert np.array_equal(f_102('tomer', 'S'), f_102('r', 'S'))
+    assert np.array_equal(f_102('tomer', 'S'), f_102('ofir', 'S'))
     # # test F103
     assert np.array_equal(np.array([1, 0, 0, 0, 0, 0, 0, 0]),
-                                    f_103('S','S','S'))
+                          f_103('S', 'S', 'S'))
     assert np.array_equal(np.array([0, 1, 0, 0, 0, 0, 0, 0]),
-                                    f_103('S','T','S'))
+                          f_103('S', 'T', 'S'))
     assert np.array_equal(np.array([0, 0, 1, 0, 0, 0, 0, 0]),
-                                    f_103('T','S','S'))
+                          f_103('T', 'S', 'S'))
     assert np.array_equal(np.array([0, 0, 0, 1, 0, 0, 0, 0]),
-                                    f_103('T','T','S'))
+                          f_103('T', 'T', 'S'))
     # # test F104
     assert np.array_equal(np.array([1, 0, 0, 0]),
-                                    f_104('S','S'))
+                          f_104('S', 'S'))
     assert np.array_equal(np.array([0, 1, 0, 0]),
-                                    f_104('T','S'))
+                          f_104('T', 'S'))
     # # test F105
     assert np.array_equal(np.array([1, 0]),
                           f_105('S'))
     assert np.array_equal(np.array([0, 1]),
                           f_105('T'))
     # test sparse vector
-    vec_list = [f_103('T','T','S'),f_105('S'),f_100('roy','T')]
-    assert  np.array_equal([[0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]],
-    sparse_vec_hstack(vec_list).todense())
+    vec_list = [f_103('T', 'T', 'S'), f_105('S'), f_100('roy', 'T')]
+    assert np.array_equal([[0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]],
+                          sparse_vec_hstack(vec_list).todense())
 
     print('PASS')

loss.py
@@ -1,20 +1,43 @@
 #!/usr/bin/env python
 
-def loss_function(v,spr_mats):
+import numpy as np
+from scipy import sparse
+
+LAMBDA = 1
+
+
+def loss_function(v, spr_mats):
     """
     loss function
     """
 
     # fake v todo -> remove
-    (tmp,_) = spr_mats[0]
+    (tmp, _) = spr_mats[0]
     v = np.ravel(tmp.todense()[0])
-
 
     linear_sum = 0
     log_sum = 0
-    for (mat,tag_idx) in spr_mats:
+    for (mat, tag_idx) in spr_mats:
         dense_dot = mat.dot(v)
         linear_sum += v[tag_idx]
         log_sum += np.log(np.sum(np.exp(dense_dot)))
 
-    return linear_sum-log_sum-(LAMBDA/2)*np.linalg.norm(v)
+    return linear_sum - log_sum - (LAMBDA / 2) * np.linalg.norm(v)
+
+
+def dloss_dv(v, spr_mats):
+    """
+    calculate the derivative of the loss function
+    """
+    feature_dim = spr_mats[0][0].shape[1]
+    vector_linear_sum = sparse.csr_matrix((1, feature_dim))
+    vector_exp_sum = sparse.csr_matrix((1, feature_dim))
+    for spr_mat, tag_idx in spr_mats:
+        # add word, tag pair feature vector to the accumulator
+        vector_linear_sum += spr_mat.getrow(tag_idx)
+
+        # calculate the softmax for each tag for current word
+        softmax_for_each_tag = np.exp(spr_mat.dot(v))
+        softmax_for_each_tag /= np.sum(softmax_for_each_tag)
+        vector_exp_sum += spr_mat.transpose().dot(softmax_for_each_tag).transpose()
+    return vector_linear_sum - vector_exp_sum - LAMBDA * v

main.py
@@ -14,15 +14,17 @@ import time
 RARE_THRESHOLD = 3
 LAMBDA = 0.1
 
+
 def load_data(train_file):
     """
     load training data
     """
     data = []
-    with open(train_file,'r') as fh:
+    with open(train_file, 'r') as fh:
         for line in fh:
             data.append([tuple(word_tag.split('_')) for word_tag in line.strip().split()])
     return data
+
 
 def vocab_and_tag_lists(data):
     """
@@ -34,7 +36,8 @@ def vocab_and_tag_lists(data):
         for word_tag_pair in sentence:
             word_set.add(word_tag_pair[0])
             tag_set.add(word_tag_pair[1])
-    return list(word_set),list(tag_set)
+    return list(word_set), list(tag_set)
+
 
 def index_sentence_word(sentence, idx):
     """
@@ -44,6 +47,7 @@ def index_sentence_word(sentence, idx):
         return None
     return sentence[idx][0]
 
+
 def index_sentence_tag(sentence, idx):
     """
     safe indexing of sentence tag 
@@ -52,7 +56,8 @@ def index_sentence_tag(sentence, idx):
         return None
     return sentence[idx][1]
 
-def extract_features(vocab_list,tag_list,data,threads):
+
+def extract_features(vocab_list, tag_list, data, threads):
     """
     extract features from training data
     """
@@ -60,13 +65,13 @@ def extract_features(vocab_list,tag_list,data,threads):
     data = data[:8]
 
     # divide data into chunks
-    sentence_batch_size = len(data)//threads
+    sentence_batch_size = len(data) // threads
     chunks = [data[idx:idx + sentence_batch_size] for idx in range(0, len(data), sentence_batch_size)]
     threads = []
     spr_mats = [[] for _ in range(len(chunks))]
 
     # run threads
-    for idx,chunk in enumerate(chunks):
+    for idx, chunk in enumerate(chunks):
         thread = Thread(target=extract_features_thread, args=(vocab_list, tag_list, chunk, spr_mats[idx]))
         thread.start()
         threads.append(thread)
@@ -78,14 +83,15 @@ def extract_features(vocab_list,tag_list,data,threads):
     # combine results
     return list(itertools.chain.from_iterable(spr_mats))
 
-def extract_features_thread(vocab_list,tag_list,data,spr_mats):
+
+def extract_features_thread(vocab_list, tag_list, data, spr_mats):
     """
     extract features from data chunk
     """
 
     # init feature classes
     f_100 = F100(vocab_list, tag_list)
-    f_101_1 = F101(vocab_list,tag_list,  1)
+    f_101_1 = F101(vocab_list, tag_list, 1)
     f_101_2 = F101(vocab_list, tag_list, 2)
     f_101_3 = F101(vocab_list, tag_list, 3)
     f_101_4 = F101(vocab_list, tag_list, 4)
@@ -104,19 +110,19 @@ def extract_features_thread(vocab_list,tag_list,data,spr_mats):
         for idx, (word, tag) in enumerate(sentence):
             spr_tag_list = []
             for tag_i in tag_list:
-                vec_list = [f_100(word,tag_i),
-                        f_101_1(word,tag_i), f_101_2(word,tag_i), f_101_3(word,tag_i), f_101_4(word,tag_i),
-                        f_102_1(word,tag_i), f_102_2(word,tag_i), f_102_3(word,tag_i), f_102_4(word,tag_i),
-                        f_103(index_sentence_tag(sentence, idx - 2), index_sentence_tag(sentence, idx - 1),tag_i),
-                        f_104(index_sentence_tag(sentence, idx - 1),tag_i),
-                        f_105(tag_i),
-                        f_100(index_sentence_word(sentence, idx - 1),tag_i), # F106
-                        f_100(index_sentence_word(sentence, idx + 1),tag_i)] # F107
+                vec_list = [f_100(word, tag_i),
+                            f_101_1(word, tag_i), f_101_2(word, tag_i), f_101_3(word, tag_i), f_101_4(word, tag_i),
+                            f_102_1(word, tag_i), f_102_2(word, tag_i), f_102_3(word, tag_i), f_102_4(word, tag_i),
+                            f_103(index_sentence_tag(sentence, idx - 2), index_sentence_tag(sentence, idx - 1), tag_i),
+                            f_104(index_sentence_tag(sentence, idx - 1), tag_i),
+                            f_105(tag_i),
+                            f_100(index_sentence_word(sentence, idx - 1), tag_i),  # F106
+                            f_100(index_sentence_word(sentence, idx + 1), tag_i)]  # F107
                 spr_tag_list.append(sparse_vec_hstack(vec_list))
-            spr_mats.append((sparse.vstack(spr_tag_list),tag_idx_dict[tag]))
+            spr_mats.append((sparse.vstack(spr_tag_list), tag_idx_dict[tag]))
 
 
-def rare(vocab_list,data):
+def rare(vocab_list, data):
     """
     remove rare words from vocab list
     """
@@ -125,13 +131,14 @@ def rare(vocab_list,data):
     for sentence in data:
         for (word, _) in sentence:
             if word in vocab_dict:
-                vocab_dict[word]+=1
+                vocab_dict[word] += 1
             else:
-                vocab_dict[word]=0
+                vocab_dict[word] = 0
 
     for word in vocab_dict:
         if vocab_dict[word] < RARE_THRESHOLD:
             vocab_list.remove(word)
+
 
 def feature_vec_len(spr_mats):
     return spr_mats[0][0].shape[1]
@@ -142,7 +149,7 @@ if __name__ == '__main__':
     train_file = None
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_file', help='training set file',default='train.wtag')
+    parser.add_argument('--train_file', help='training set file', default='train.wtag')
     args = parser.parse_args()
 
     if args.train_file:
@@ -158,20 +165,20 @@ if __name__ == '__main__':
 
     # remove rare words from vocabulary
     print('remove rare words from vocabulary')
-    rare(vocab_list,data)
+    rare(vocab_list, data)
 
     # extract features from training data
     print('extract features from training data')
     start = time.time()
-    spr_mats = extract_features(vocab_list,tag_list,data,8)
-    print('extract time: ',time.time()-start)
+    spr_mats = extract_features(vocab_list, tag_list, data, 8)
+    print('extract time: ', time.time() - start)
 
     # training
     print('run training')
     start = time.time()
 
-    x_0 = np.zeros(feature_vec_len(spr_mats)) # initial guess shape (n,)
+    x_0 = np.zeros(feature_vec_len(spr_mats))  # initial guess shape (n,)
     print(x_0)
-    #v = optimize.minimize(loss_function,x0=x_0,args=spr_mat,method='BFGS',jac=dloss_dv,maxiter=10)
-    print('training time: ',time.time()-start)
-    #print('loss: ',loss_function(spr_arr,spr_arr))
+    # v = optimize.minimize(loss_function,x0=x_0,args=spr_mat,method='BFGS',jac=dloss_dv,maxiter=10)
+    print('training time: ', time.time() - start)
+    # print('loss: ',loss_function(spr_arr,spr_arr))